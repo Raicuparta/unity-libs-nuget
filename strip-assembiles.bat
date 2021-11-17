c86197d1b4075d968625b24dc5771f3cc9f9d603
Raicuparta <raicuparta@gmail.com>
2021-11-17 17:39:42 +0100

    Add publicized assemblies to script

--- strip-assembiles.bat
+++ strip-assembiles.bat
@@ -2,9 +2,23 @@
 set exePath=%1
 echo exePath: %exePath% 
 
-set managedPath=%exePath:OuterWilds.exe=OuterWilds_Data\Managed% 
+@REM Remove quotes
+set exePath=%exePath:"=%
+
+set managedPath=%exePath:OuterWilds.exe=OuterWilds_Data\Managed%
 echo managedPath: %managedPath%
 
-%~dp0\tools\NStrip.exe %managedPath% -o "%~dp0\package\lib\net46"
+set outPath=%~dp0\package\lib\net46
+set publicizedStripParams=-cg -p --cg-exclude-events
+
+@REM Strip all assembiles, but keep them private.
+%~dp0\tools\NStrip.exe "%managedPath%" -o %outPath%
+
+
+echo %~dp0\tools\NStrip.exe "%managedPath%\Assembly-CSharp.dll" -o "%outPath%" %publicizedStripParams%
+
+@REM Strip and publicize Assembly-CSharp.
+%~dp0\tools\NStrip.exe "%managedPath%\Assembly-CSharp.dll" -o "%outPath%\Assembly-CSharp.dll" %publicizedStripParams%
+%~dp0\tools\NStrip.exe "%managedPath%\Assembly-CSharp-firstpass.dll" -o "%outPath%\Assembly-CSharp-firstpass.dll" %publicizedStripParams%
 
 pause
